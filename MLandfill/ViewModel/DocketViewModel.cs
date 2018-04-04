b786cd2425e90493ca6cea788b688c61cc6ea0a4
Ananda <anawicks@outlook.com>
2018-04-04 16:31:32 -0600

    April 4 th Test

--- MLandfill/ViewModel/DocketViewModel.cs
+++ MLandfill/ViewModel/DocketViewModel.cs
@@ -5,6 +5,7 @@ using System.Web;
 using MLandfill.Core.Domain;
 using MLandfill.Models;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace MLandfill.ViewModel
 {
@@ -15,15 +16,19 @@ namespace MLandfill.ViewModel
         [Key]
         public int DocketId { get; set; }
        [Required]
+       [DisplayName("Docket No")]
         public string DocketNo { get; set; }
-       
+        [DisplayName("Approval Code")]
         public string WasteApprovalCode { get; set; }
         public int InvoiceeId { get; set; }
       
         public int TurckCompanyId { get; set; }
+
+        [DisplayName("Driver")]
         public string DriverName { get; set; }
+        [DisplayName("Destinated For")]
         public string DestinatedFor { get; set; }
-      
+        [DisplayName("Scale Ticket")]
         public string ScaleTicket { get; set; }
         
         public decimal Gross { get; set; }
@@ -32,11 +37,13 @@ namespace MLandfill.ViewModel
         public decimal Net { get; set; }
         public string Cell { get; set; }
         public string Grid { get; set; }
+        [DisplayName("Grid No")]
         public string GridNo { get; set; }
         public string Elevation { get; set; }
 
         
         [DataType(DataType.Date)]
+        [DisplayName("Received Date")]
         //[DisplayFormat(DataFormatString = "{0:dd-mm-yyyy}", ApplyFormatInEditMode = true)]
         //public DateTime DateReceived { get; set; }
         public DateTime DateReceived
@@ -48,26 +55,37 @@ namespace MLandfill.ViewModel
             set { _createdOn = value; }
         }
         public string Memo { get; set; }
+        [DisplayName("Invoice No")]
         public string InvoiceNo { get; set; }
+        [DisplayName("Load Received Date")]
         public DateTime LoadReceivingDate { get; set; }
-         
+        [DisplayName("DocketNo")]
         public string TruckCompName { get; set; }
+        [DisplayName("Address")]
         public string TruckCompAddr { get; set; }
+        [DisplayName("City")]
         public string TruckCompCity { get; set; }
+        [DisplayName("Province")]
         public string TruckCompProv { get; set; }
+        [DisplayName("Postal Code")]
         public string TruckCompPostal { get; set; }
+        [DisplayName("Phone")]
         public string TruckCompPhone { get; set; }
         public int TruckCompId { get; set; }
         public int WApGeneratorId { get; set; }
+        [DisplayName("DocketNo")]
         public string WApWasteDescrip { get; set; }
         public int WApSubId { get; set; }
+        [DisplayName("Approval Code")]
         public string WApApprovalcode { get; set; }
         [Required]
         public int   WApApprovalId { get; set; }
-
+        [DisplayName("Generator")]
         public string GeneratorName { get; set; }
+        [DisplayName("Substance")]
         public string SubstanceName { get; set; }
         public int GenerLocationId { get; set; }
+        [DisplayName("Location Lsd")]
         public string GenerLocationLsd { get; set; }
         public IEnumerable<tblGenerator> tblGenerators { get; set; }
 
